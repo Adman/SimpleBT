9f531bc0ef736780103e75a1d06c2b5117662c6d
Ado Matejov <a.matejov@centrum.sk>
2011-10-12 17:50:19 +0200

    IDs, and images loaded

---
+++ obrazky.py
@@ -0,0 +1,33 @@
+
+# images
+upleft = "icons/upleft.png"
+left = "icons/left.png"
+downleft = "icons/downleft.png"
+up = "icons/up.png"
+centre = "icons/centre.png"
+down = "icons/down.png"
+upright = "icons/upright.png"
+right = "icons/right.png"
+downright = "icons/downright.png"
+
+# toggle images
+upleftblue = "icons/upleftblue.png"
+leftblue = "icons/leftblue.png"
+downleftblue = "icons/downleftblue.png"
+upblue = "icons/upblue.png"
+centreblue = "icons/centreblue.png"
+downblue = "icons/downblue.png"
+uprightblue = "icons/uprightblue.png"
+rightblue = "icons/rightblue.png"
+downrightblue = "icons/downrightblue.png"
+
+# IDs
+upleftID = 1
+leftID = 2
+downleftID = 3
+upID = 4
+centreID = 5
+downID = 6
+uprightID = 7
+rightID = 8
+downrightID = 9
